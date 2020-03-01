898d60a7f7a356cbcdac36b1f5cb22a6152610ee
Ba Matrai <matrai.barnabas1@gmail.com>
2020-03-01 16:48:14 +0100

    can show bishop valid moves

--- chessEasy/chessEasy/MainWindow.xaml.cs
+++ chessEasy/chessEasy/MainWindow.xaml.cs
@@ -82,6 +82,12 @@ namespace chessEasy
                         .Cast<Border>()
                         .Where(child => Grid.GetColumn(child) == columnNumber || Grid.GetRow(child) == rowNumber);
                 }
+                else if (chessPiece.Source.ToString().Contains("bishop"))
+                {
+                    borders = chessBoard.Children
+                        .Cast<Border>()
+                        .Where(child => Math.Abs(Grid.GetColumn(child) - columnNumber) == Math.Abs(Grid.GetRow(child) - rowNumber));
+                }
             }
             else
             {
@@ -96,6 +102,12 @@ namespace chessEasy
                     borders = chessBoard.Children
                         .Cast<Border>()
                         .Where(child => Grid.GetColumn(child) == columnNumber || Grid.GetRow(child) == rowNumber);
+                }
+                else if (chessPiece.Source.ToString().Contains("bishop"))
+                {
+                    borders = chessBoard.Children
+                        .Cast<Border>()
+                        .Where(child => Math.Abs(Grid.GetColumn(child) - columnNumber) == Math.Abs(Grid.GetRow(child) - rowNumber));
                 }
             }
 
