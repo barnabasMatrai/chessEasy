4da3e097b6a1c880a19fb9e596f3f540d3c2d8c0
Matrai Ba <matrai.barnabas1@gmail.com>
2020-04-20 14:57:49 +0200

    create Chesspiece class

--- chessEasy/chessEasy/Models/ChessPiece.cs
+++ chessEasy/chessEasy/Models/ChessPiece.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace chessEasy.Models
 {
@@ -12,5 +14,21 @@ namespace chessEasy.Models
         protected string ImagePath;
         protected Color Color;
         protected Point Coordinates;
+
+        protected abstract void MoveChesspiece();
+        protected abstract IEnumerable<Border> GetValidMoves();
+        protected abstract IEnumerable<Border> RemoveInvalidMoves();
+        protected void ShowValidMoves()
+        {
+            IEnumerable<Border> borders = GetValidMoves();
+
+            if (borders != null)
+            {
+                foreach (Border border in borders)
+                {
+                    border.Background = Brushes.LightGreen;
+                }
+            }
+        }
     }
 }
